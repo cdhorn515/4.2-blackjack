7a0b792fd3540fecc015b05328ac04c364f7d4ca
Christina Cornell <cdhorn515@gmail.com>
2017-06-15 01:35:59 -0400

    assessment finished, solution is working. test script running error but asserts pass when passed into main js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,53 @@
-/*
-   Implement a Blackjack hand value calculator.
+(function() {
 
-   Open up the `index.html` file and your console
-   to watch the assertions pass as you write your code.
-
-   Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to
-   write your logic.
-*/
-// function assert(hand, value) {
-//   let output = handValue(hand);
-// assert(["2", "2", "8"], 12);
+  'use strict';
 
 
-function handValue(hand) {
-  var cardsInHand = [0];
-  var cardTotal = 0;
-  for (var i = 0; i < hand.length; i++) {
-    if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
-      hand[i] = "10";
-    } else if (hand[i] === "A") {
-      // if (cardTotal > 18) {
-        hand[i] === 11;
-       } //else if (hand[i] === "A" && cardTotal < ) {
-      //
-      // }
+  /*
+     Implement a Blackjack hand value calculator.
+
+     Open up the `index.html` file and your console
+     to watch the assertions pass as you write your code.
+
+     Also remember, that the parameter `hand` will be an array, so
+     you'll need to parse through that first before you can start to
+     write your logic.
+  */
+  function handValue(hand) {
+    var total = 0;
+    var cardsInHand = [];
+    var holdingAces = 0;
+    for (var i = 0; i < hand.length; i++) {
+      if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
+        hand[i] = "10";
+      } else if (hand[i] === "A") {
+        holdingAces++;
+        hand[i] = 11;
+      }
     }
-
-  // else if (hand[i] === "A") {
-  //   hand[i] = "11";
-  // }
-
-
-for (var i = 0; i < hand.length; i++) {
-  cardsInHand[i] = parseInt(hand[i]);
-  cardTotal = cardTotal + cardsInHand[i];
-  if (cardTotal > 18 ) {
-    cardTotal = cardTotal - 10;
+    var cardTotal = 0;
+    for (var i = 0; i < hand.length; i++) {
+      cardsInHand[i] = parseInt(hand[i]);
+      console.log(cardsInHand);
+      cardTotal += cardsInHand[i];
+    }
+    if (cardTotal > 18) {
+      cardTotal = cardTotal - (10 * holdingAces);
+      console.log(holdingAces);
+    }
+    console.log(cardTotal);
+    return cardTotal;
   }
-}
+
+  //  handValue(["8", "7", "A", "A"]);
 
 
-console.log(cardTotal);
-return cardTotal;
 
-return;
-}
-handValue(["8", "K", "A"]);
+  /* -----  Hints ------
 
-/* -----  Hints ------
+  1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
+  K, Q, J ==> Worth 10
+  A       ==> Worth 1 or 11
 
-1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
-K, Q, J ==> Worth 10
-A       ==> Worth 1 or 11
-
-*/
+  */
+})();
